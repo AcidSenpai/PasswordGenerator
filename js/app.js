efe28278cd38c034937d662f0cb863e5603f8869
AcidSenpai <maxime.averty.pro@gmail.com>
2024-03-01 18:58:45 +0100

    Javascript finished but the strong indicator is a bit weird. I can't do better for now

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@ const checkLowercase = document.getElementById("checkbutton_lowercase")
 const checkNumbers = document.getElementById("checkbutton_numbers")
 const checkSymbols = document.getElementById("checkbutton_symbols")
 const generateBtn = document.getElementById("settings_generate")
+const copyKeyboard = document.getElementById("copyKeyboard")
+
+const allBar = document.querySelectorAll(".bar")
+
+const bar1 = document.getElementById("bar1")
+const bar2 = document.getElementById("bar2")
+const bar3 = document.getElementById("bar3")
+const bar4 = document.getElementById("bar4")
 
 let containUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let containLowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -57,9 +65,47 @@ const generatePassword = () => {
             password += passwordChars.charAt(randomChars)
         }
         inputPassword.value = password
+        rankPassword(password)
     }
 
 }
+
+const setBarStyle = (bar, style) => {
+    bar.classList.remove("bar-low", "bar-medium", "bar-strong");
+    bar.classList.add(`bar-${style}`);
+}
+
+const barReset = (bars) => {
+    bars.forEach(bar => {
+        bar.classList.remove("bar-medium", "bar-strong");
+    })
+}
+
+const rankPassword = (p) => {
+    if(p.length <= 6 && !p.includes(containNumbers) && !p.includes(containSymbols)) {
+        setBarStyle(bar1, "low")
+        barReset([bar2, bar3, bar4])
+    }else if(p.length <= 10 && !p.includes(containNumbers) && !p.includes(containSymbols)){
+        setBarStyle(bar1, "medium")
+        setBarStyle(bar2, "medium")
+        barReset([bar3, bar4])
+    }else if(p.length >= 14) {
+        setBarStyle(bar1, "strong")
+        setBarStyle(bar2, "strong")
+        setBarStyle(bar3, "strong")
+        setBarStyle(bar4, "strong")
+    }
+}
+
+
+copyKeyboard.addEventListener("click", () => {
+    inputPassword.select()
+    try {
+        document.execCommand("copy")
+    } catch (err){
+        console.error("Erreur lors de la copie du mot de pass", + err)
+    }
+})
 
 generateBtn.addEventListener("click", () => {
     generatePassword()
